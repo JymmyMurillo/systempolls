79a81e1f8c6434e0aa14976c7fa01c81b543d8f2
Jymmy Murillo <murillojymmy@gmail.com>
2022-05-04 09:41:30 -0500

    Preparing to deploy to Heroku

--- systempolls/settings.py
+++ systempolls/settings.py
@@ -14,7 +14,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
-
+from dotenv import load_dotenv
 from django import conf
 from whitenoise import WhiteNoise
 
@@ -27,7 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # g6e
-SECRET_KEY = 'django-insecure-ffxy @$wsvs3!u ^ ^fm = e!mm % hts7_*&o30)t(@r*3!y3x5_'
+#SECRET_KEY = 'django-insecure-ffxy @$wsvs3!u ^ ^fm = e!mm % hts7_*&o30)t(@r*3!y3x5_'
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
