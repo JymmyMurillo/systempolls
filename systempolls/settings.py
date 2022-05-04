6924d9c5c84cda5b3b123997f25a8cf458f9eff1
Jymmy Murillo <murillojymmy@gmail.com>
2022-05-03 23:24:44 -0500

    Preparing to deploy to Heroku

--- systempolls/settings.py
+++ systempolls/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'systempolls.wsgi.application'
 
 import dj_database_url
 from decouple import config
-DATABASE = {
+DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL')
     )
